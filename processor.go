3fd088c6f21b9b3139bd902b911188593535dbbe
kiarash8112 <kiarash8112hajian@gmail.com>
2023-11-08 09:40:16 -0500

    add payment processor

---
+++ processor.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"context"
+
+	"github.com/lovoo/goka"
+)
+
+var (
+	payment_group goka.Group = "payment_group"
+	booking_group goka.Group = "booking_group"
+)
+
+func paymentProcess(ctx goka.Context, msg interface{}) {
+	var p *PaymentService
+	if val := ctx.Value(); val != nil {
+		p = val.(*PaymentService)
+	} else {
+		p = new(PaymentService)
+	}
+
+	ctx.SetValue(p)
+}
+
+func runPaymentProcessor(initialized chan struct{}) {
+	g := goka.DefineGroup(payment_group,
+		goka.Input(payment_topic, new(PaymentCodec), paymentProcess),
+		goka.Persist(new(PaymentCodec)),
+	)
+	p, err := goka.NewProcessor(brokers,
+		g,
+		goka.WithTopicManagerBuilder(goka.TopicManagerBuilderWithTopicManagerConfig(tmc)),
+		goka.WithConsumerGroupBuilder(goka.DefaultConsumerGroupBuilder),
+	)
+	if err != nil {
+		panic(err)
+	}
+
+	p.Run(context.Background())
+}
