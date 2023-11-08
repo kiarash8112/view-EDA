0214fb604d12df921e7b2ee171fdbfa5ee8abf3d
kiarash8112 <kiarash8112hajian@gmail.com>
2023-11-08 09:40:33 -0500

    add booking processor

--- processor.go
+++ processor.go
@@ -38,3 +38,31 @@ func runPaymentProcessor(initialized chan struct{}) {
 
 	p.Run(context.Background())
 }
+
+func BookingProcess(ctx goka.Context, msg interface{}) {
+	var b *BookingService
+	if val := ctx.Value(); val != nil {
+		b = val.(*BookingService)
+	} else {
+		b = new(BookingService)
+	}
+
+	ctx.SetValue(b)
+}
+
+func runBookingProcessor(initialized chan struct{}) {
+	g := goka.DefineGroup(payment_group,
+		goka.Input(payment_topic, new(BookingCodec), paymentProcess),
+		goka.Persist(new(BookingCodec)),
+	)
+	p, err := goka.NewProcessor(brokers,
+		g,
+		goka.WithTopicManagerBuilder(goka.TopicManagerBuilderWithTopicManagerConfig(tmc)),
+		goka.WithConsumerGroupBuilder(goka.DefaultConsumerGroupBuilder),
+	)
+	if err != nil {
+		panic(err)
+	}
+
+	p.Run(context.Background())
+}
